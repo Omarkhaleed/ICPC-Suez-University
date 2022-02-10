6b25f7fa176a7199aeb387323aa3bd13d9e8824b
Omar khalid <86879244+Omarkhaleed@users.noreply.github.com>
2022-02-10 20:50:22 +0200

    Create 2- A. Mahmoud and Longest Uncommon Subsequence.cpp

---
+++ Sheet-Four/2- A. Mahmoud and Longest Uncommon Subsequence.cpp
@@ -0,0 +1,8 @@
+string s1, s2;
+    cin >> s1>>s2;
+    if (s1.size() != s2.size())
+        cout << max(s1.size(), s2.size()) << endl;
+    else if (s1 == s2)
+        cout << -1 << endl;
+    else
+        cout << s1.size() << endl;
