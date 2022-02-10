a5c33832479460e6e422e7c62219bbcbbcd64dc3
Omar khalid <86879244+Omarkhaleed@users.noreply.github.com>
2022-02-10 20:02:39 +0200

    Update 7- A. Petya and Strings.cpp

--- Sheet-one/7- A. Petya and Strings.cpp
+++ Sheet-one/7- A. Petya and Strings.cpp
@@ -4,3 +4,29 @@ string convert(string s) {
 		});
 	return s;
 }
+
+string s1, s2;
+	cin >> s1 >> s2;
+	string s1new=convert(s1);
+	string s2new=convert(s2);
+	int flag = 0;
+	int n = s1.size();
+	
+	int i = 0;
+	
+	
+		for (int j = 0; j < n; j++) {
+			if (s1new[j] < s2new[j]) {
+				flag = -1;
+				break;
+			}
+			else if (s1new[j] > s2new[j]) {
+				flag = 1;
+				break;
+			}
+			else
+				flag = 0;
+			
+	
+	    } 
+	cout << flag << "\n";
