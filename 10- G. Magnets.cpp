74187e02e0e28f2059eb2ee81db818c7e261ee49
Omar khalid <86879244+Omarkhaleed@users.noreply.github.com>
2022-02-10 20:05:05 +0200

    Create 10- G. Magnets.cpp

---
+++ 10- G. Magnets.cpp
@@ -0,0 +1,11 @@
+  int n;
+    cin>>n;
+    int magnets[n],count=1;
+    for (int i=0; i<n;i++)
+        cin>>magnets[i];
+    for (int j=0 ;j<n-1;j++){
+        if (magnets[j]!=magnets[j+1])
+            count++;
+    }   
+    cout<<count<<endl;
+	 
