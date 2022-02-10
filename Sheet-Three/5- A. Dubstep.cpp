23dc36c07c37875dcb76dfa826978dc75549c442
Omar khalid <86879244+Omarkhaleed@users.noreply.github.com>
2022-02-10 20:35:59 +0200

    Create 5- A. Dubstep.cpp

---
+++ Sheet-Three/5- A. Dubstep.cpp
@@ -0,0 +1,17 @@
+
+string s;
+	bool flag = true;
+	cin >> s;
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == 'W' && s[i + 1] == 'U' && s[i + 2] == 'B') {
+			i += 2;
+			if (!flag) {
+				cout << " ";
+			}
+			continue;
+		}
+		else {
+			flag = false;
+			cout << s[i];
+		}
+	}
